16f82b9a9e6b9b99acebd3972a7969683f2b1ad2
juniuszhou <junius.zhou@gmail.com>
2019-08-25 09:20:07 +0800

    Update runtime after merge upstream.

--- litentry/docker/build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cp ../../target/release/litentry ./
-docker build -t litentry-substrate .
-rm litentry
-
-
-
--- litentry/docker/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-RUST_LOG=debug RUST_BACKTRACE=1 /usr/local/bin/litentry-substrate --dev --rpc-cors=all \
---ws-external=true --rpc-external=true --base-path=/substrate/data/litentry
